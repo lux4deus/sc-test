6ccab9fb678c3160b87c24dc66849e917f87d231
lux4deus <lux4deus@gmail.com>
2016-06-05 13:30:34 +0300

    initial

---
+++ src/AppBundle/Controller/ServersController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ServersController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction($serverID)
+    {
+        var_dump($serverID);
+        $request = new Request();
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+}
